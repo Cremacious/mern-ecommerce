eb5dc7dea9eafa95267b76d8995f103f9fe090bf
cremacious <chrismackall3@gmail.com>
2025-06-19 15:04:45 -0400

    New user schema created with bcrypt functionality

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signUp = async (req, res) => {
+  res.send('Sign up route');
+};
+export const login = async (req, res) => {
+  res.send('login route');
+};
+export const logout = async (req, res) => {
+  res.send('logout route');
+};
