42c3285983716a45d99dfd2eb117058d1ff58a0b
cremacious <chrismackall3@gmail.com>
2025-06-19 15:53:17 -0400

    Successful user creation in database

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,21 @@
+import User from '../models/user.model.js';
+
 export const signUp = async (req, res) => {
-  res.send('Sign up route');
+  try {
+    const { email, password, name } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+    const user = await User.create({ name, email, password });
+    return res.status(201).json({ user, message: 'You created successfully' });
+  } catch (error) {
+    console.log('Error in signup controller', error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
+
 export const login = async (req, res) => {
   res.send('login route');
 };
