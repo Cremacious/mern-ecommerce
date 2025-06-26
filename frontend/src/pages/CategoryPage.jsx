8c4ec39ea1601ac375e90bd63dda3de930c98aa5
Cremacious <chrismackall3@gmail.com>
2025-06-26 18:18:27 -0400

    Added pages for purchase success and cancel, edited styling for cart item

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useProductStore } from '../stores/useProductStore';
 import { useParams } from 'react-router';
+
 import ProductCard from '../components/ProductCard';
 
 const CategoryPage = () => {
@@ -12,12 +13,25 @@ const CategoryPage = () => {
     fetchProductsByCategory(category);
   }, [fetchProductsByCategory, category]);
 
+  console.log('products:', products);
   return (
     <div className="min-h-screen">
       <div className="relative z-10 max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-        <h1>{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
+        <h1
+          className="text-center text-4xl sm:text-5xl font-bold text-emerald-400 mb-8"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          {category.charAt(0).toUpperCase() + category.slice(1)}
+        </h1>
 
-        <div>
+        <div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+        >
           {products?.length === 0 && (
             <h2 className="text-3xl font-semibold text-gray-300 text-center col-span-full">
               No products found
